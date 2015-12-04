b9cc1b960c52d2f39ae26dc1ef7e9c9469997f8c
Yasuhiro Tanaka <frisco999@mac.com>
2015-12-04 23:21:32 +0900

    Initial Commit

---
+++ Me Electricity Bills/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Me Electricity Bills
+//
+//  Created by Yasu Tanaka on 04/12/2015.
+//  Copyright © 2015 Yasuhiro Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
